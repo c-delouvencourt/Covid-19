47f5b700400d961aa6168c096544e631a9270ef1
NocturneLab <c.delouvencourt@nocturne.app>
2020-03-16 11:10:13 +0100

    Updates

---
+++ precache-manifest.9942adf0e9a3230839210f743a82b2fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd9b74aaf321ffa4dea8b51899d943a",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "b907406c6b1f810cad60",
+    "url": "/Covid-19/static/css/main.ac718a1d.chunk.css"
+  },
+  {
+    "revision": "b3c18bdf8fb28aa20120",
+    "url": "/Covid-19/static/js/2.6638e487.chunk.js"
+  },
+  {
+    "revision": "0165c4003f503d2a8c06c5ca79ae3140",
+    "url": "/Covid-19/static/js/2.6638e487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b907406c6b1f810cad60",
+    "url": "/Covid-19/static/js/main.01e4d01c.chunk.js"
+  },
+  {
+    "revision": "48513976be5079156807",
+    "url": "/Covid-19/static/js/runtime-main.52b0cec8.js"
+  }
+]);
